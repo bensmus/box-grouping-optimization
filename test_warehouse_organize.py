6e3bf0d79b81a139e3b74f64018e6c56c4856758
Ben Smus <bensmus@gmail.com>
2024-10-17 17:32:29 -0700

    18 cells, 3 group size: does not complete in 1.5h

--- test_warehouse_organize.py
+++ test_warehouse_organize.py
@@ -1,4 +1,20 @@
-from warehouse_organize import compute_group_cost
+from warehouse_organize import compute_all_groupings, compute_group_cost
+
+######## COMPUTE GROUPINGS ########
+
+assert compute_all_groupings([1, 2, 3, 4], 2) == {
+    frozenset({
+        frozenset({1, 2}), frozenset({3, 4})
+    }),
+    frozenset({
+        frozenset({1, 3}), frozenset({2, 4})
+    }),
+    frozenset({
+        frozenset({1, 4}), frozenset({2, 3})
+    })
+}
+
+######## COMPUTE GROUP COST ########
 
 """
 cells:
@@ -14,4 +30,24 @@ group = frozenset({(1, 3), (2, 2), (2, 3)}) # Extra group cell does not contribu
 assert compute_group_cost(cells, group) == 1, "Wrong group cost"
 
 group = frozenset({(2, 0), (1, 2), (2, 2)})
+assert compute_group_cost(cells, group) == 0, "Wrong group cost"
+
+"""
+cells:
+X
+XXXX
+XXXX
+XXXX
+"""
+cells = [(0, 0)] + [(i, j) for i in range(1, 4) for j in range(4)]
+group = frozenset({(3, 0), (3, 1), (3, 2)})
+assert compute_group_cost(cells, group) == 7, "Wrong group cost"
+
+"""
+cells:
+XX
+XX
+"""
+cells = [(i, j) for i in range(2) for j in range(2)]
+group = frozenset({(0, 0), (0, 1)})
 assert compute_group_cost(cells, group) == 0, "Wrong group cost"
--- warehouse_organize.py
+++ warehouse_organize.py
@@ -5,22 +5,23 @@ def exclude(ar, elems):
 
 def compute_all_groupings(ar, group_size):
     """
-    compute_all_groupings([1, 2, 3, 4], 2) gives [{{2, 4}, {1, 3}}, {{1, 4}, {2, 3}}, {{3, 4}, {1, 2}}]
-    but all the sets are frozensets.
+    A grouping is defined as a subdivision of `ar` into groups where
+    all of the groups have size `group_size`. So if `ar` is [A, B, C, D, E, F],
+    one possible grouping with `group_size` 3 would be { {A, B, E}, {C, D, F} }.
     """
-    # Use a set to ensure that all the partitions are unique.
-    partitions = set()
-    # A recursive function that updates partitions.
+    # Use a set to ensure that all the groupings are unique.
+    groupings = set()
+    # A recursive function that updates groupings.
     # `accumulated` is the current labeling, `remaining` is what remains to be labeled.
     def recursive(accumulated, remaining):
         for pair in itertools.combinations(remaining, group_size):
             next_accumulated = accumulated.union({frozenset(pair)}) # `next_accumulated` is a set of frozensets.
             next_remaining = exclude(remaining, pair)
             if next_remaining == []: # We fully divided the printers up.
-                partitions.add(frozenset(next_accumulated)) # Sets aren't hashable, so frozenset it.
+                groupings.add(frozenset(next_accumulated)) # Sets aren't hashable, so frozenset it.
             recursive(next_accumulated, next_remaining)
     recursive(set(), ar)
-    return list(partitions) # For consistent iteration.
+    return groupings
 
 def compute_group_cost(cells, group):
     # `cells` and `group` are both collections of tuples.
@@ -60,11 +61,12 @@ def compute_grouping_cost(cells, grouping):
     return grouping_cost
 
 def compute_best_groupings(cells, grouping_size):
-    groupings = compute_all_groupings(cells, grouping_size)
+    groupings = list(compute_all_groupings(cells, grouping_size)) # list() For consistent iteration.
     grouping_costs = []
     for grouping in groupings:
         grouping_costs.append(compute_grouping_cost(cells, grouping))
     best_groupings = [grouping for i, grouping in enumerate(groupings) if grouping_costs[i] == min(grouping_costs)]
+    print(min(grouping_costs))
     return best_groupings
 
 """
@@ -73,3 +75,18 @@ XX
 XXXX
 XXXXXX
 """
+# Runs in 3 seconds.
+# cells = [(0, j) for j in range(2)] + [(1, j) for j in range(4)] + [(2, j) for j in range(6)]
+# print(compute_best_groupings(cells, 3))
+
+"""
+Groups of 3 for:
+XX
+XXXX
+XXXX
+XXXX
+XXXX
+"""
+# Does not complete in 1.5 hours.
+# cells = [(0, 0), (0, 1)] + [(i, j) for i in range(1, 5) for j in range(4)]
+# print(compute_best_groupings(cells, 3))
